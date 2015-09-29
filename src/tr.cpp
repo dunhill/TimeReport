3a8b865052fb38e23e773befba0f314650283c79
Alexey Klymenko <asklymenko@ukr.net>
2015-09-29 23:17:05 +0200

    Added launch arguments. Added writing test staff to file specified in 1st argument

--- src/tr.cpp
+++ src/tr.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
     std::cout << "TimeReport tool" << std::endl;
+    std::cout << "Path to executable: " << argv[0] << std::endl;
+    if (argc == 1)
+    {
+        std::cout << "No parameters specified" << std::endl;
+    }
+    else
+    {
+        std::cout << "Parameters:" << std::endl;
+        for (int i = 1; i < argc; ++i)
+        {
+            std::cout << i << ": " << argv[i] << std::endl;
+        }
+
+        std::string file_name(argv[1]);
+        std::ofstream out_file(file_name.c_str(), std::ios_base::out | std::ios_base::app);
+        out_file << "test" << std::endl;
+        out_file.close();
+    }
     return 0;
 }
